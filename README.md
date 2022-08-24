3fe8e8ca698aa9cbaee48a507d69057f8f503b6a
Keith Levi Lumanog <keith@webninjamobile.com>
2022-08-24 23:52:23 +0800

    update

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
-#Signalytics JOI package
+# JOI package
 
 Joi custom validations includes
 1. ObjectId
 2. List with Pagination
+3. Search Validation
+4. Filtering with pagination
 
-#Requirements
+# Requirements
 1. GitHub personal tokens
 2. You must be logged-in in order to run the commands below
 
-##Commands
+## Commands
 
 ### Auto Patching Version
 `npm version patch`
--- src/core.validator.ts
+++ src/core.validator.ts
@@ -11,3 +11,63 @@ export const listPaginationValidator = {
     page: Joi.number(),
   }),
 };
+
+const filter = function (isStrict = false, values) {
+  const schema = Joi.array()
+    .items(
+      Joi.object({
+        key: Joi.string()
+          .valid(...values)
+          .required(),
+        value: Joi.string().required(),
+      })
+    )
+    .required();
+
+  if (isStrict) {
+    schema.length(values.length);
+  }
+
+  return schema;
+};
+
+export const filterValidator = function (isStrict = false, ...validFilterKeys: string[]) {
+  return {
+    schema: Joi.object({
+      filters: filter(isStrict, validFilterKeys),
+      sort: Joi.object(),
+      page: Joi.number(),
+      limit: Joi.number(),
+    }),
+  };
+};
+
+export const searchValidator = function (isStrict = true, ...validValues: string[]) {
+  const key = isStrict ? Joi.string().valid(...validValues) : Joi.string().allow(...validValues);
+  return {
+    schema: Joi.object({
+      key: key.required(),
+      value: Joi.string().required(),
+    }),
+  };
+};
+
+export const searchFilterValidator = function ({
+  validSearchKeys = [''],
+  validFilterKeys = [''],
+  isStrict = false,
+}: {
+  validSearchKeys: string[];
+  validFilterKeys: string[];
+  isStrict?: boolean;
+}) {
+  return {
+    schema: Joi.object({
+      key: Joi.string()
+        .allow(...validSearchKeys)
+        .required(),
+      value: Joi.string().required(),
+      filters: filter(isStrict, validFilterKeys),
+    }),
+  };
+};
